138fd04322e31f494739a629b12c4e5989667f47
LOMADA SIVA GANGI REDDY <153607527+shivareddy2002@users.noreply.github.com>
2026-05-05 16:28:24 +0530

    Change response display to show JSON data

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,4 +65,4 @@ if time_to_insert:
         st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
